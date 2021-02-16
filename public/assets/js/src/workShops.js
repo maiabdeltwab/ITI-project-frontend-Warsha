a3c81a6c49ecf13695a34cffc25bd005841dee9f
yomna-hosny <yomna.hosny@yahoo.com>
2021-02-16 04:53:00 +0200

    firebase as backend

--- public/assets/js/src/workShops.js
+++ public/assets/js/src/workShops.js
@@ -1,100 +1,122 @@
 var data;
 var rate = "", j = 0;
+var i = 0;
+// function readTextFile(file, callback) {
+//     var rawFile = new XMLHttpRequest();
+//     rawFile.overrideMimeType("application/json");
+//     rawFile.open("GET", file, true);
+//     rawFile.onreadystatechange = function () {
+//         if (rawFile.readyState === 4 && rawFile.status == "200") {
+//             callback(rawFile.responseText);
+//         }
+//     }
+//     rawFile.send(null);
+// }
 
-function readTextFile(file, callback) {
-    var rawFile = new XMLHttpRequest();
-    rawFile.overrideMimeType("application/json");
-    rawFile.open("GET", file, true);
-    rawFile.onreadystatechange = function () {
-        if (rawFile.readyState === 4 && rawFile.status == "200") {
-            callback(rawFile.responseText);
-        }
-    }
-    rawFile.send(null);
-}
+var firebaseConfig = {
+    apiKey: "AIzaSyAC0G0nvAK7KbKx2qgkyGJ16jORzlzyE-Y",
+    authDomain: "carworkshop-7ec37.firebaseapp.com",
+    databaseURL: "https://carworkshop-7ec37-default-rtdb.firebaseio.com",
+    projectId: "carworkshop-7ec37",
+    storageBucket: "carworkshop-7ec37.appspot.com",
+    messagingSenderId: "640153744311",
+    appId: "1:640153744311:web:27c8a4e0fe189c40136e32",
+    measurementId: "G-641RNWK139"
+};
+
+// Initialize Firebase;
+firebase.initializeApp(firebaseConfig);
+var db = firebase.firestore();
+var anlaytics = firebase.analytics();
+
 
 function loadData() {
+    i = 0;
     document.getElementById("workShops").innerHTML = "";
 
-    readTextFile("Data.json", function (text) {
-        var data = JSON.parse(text);
-        for (var i = 0; i < data.length; i++) {
-
+    db.collection("workshop").orderBy("id").get().then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+            var data = doc.data();
             rate = "";
             j = 0;
 
             if (i % 2 == 0) {
-                var tr = document.createElement('div');
+                tr = document.createElement('div');
                 tr.className = "row";
             }
 
             var td = document.createElement('div');
             td.className = "col-md-6 WshopTd";
 
-            for (j = 0; j < parseInt(data[i].rate); j++)
+            for (j = 0; j < parseInt(data.rate); j++)
                 rate += "<i class='fa fa-star rate'></i>"
-            if (data[i].rate % 1 != 0) {
+            if (data.rate % 1 != 0) {
                 rate += "<i class='fa fa-star-half-empty rate'></i>"
                 j++;
             }
             for (j; j < 5; j++)
                 rate += "<i class='fa fa-star-o rate'></i>"
 
-            //td.innerHTML = " <td class='WshopTd'> <div class='card'>   <div class='logo' style='background-image:" + data[i].icon + "'>    </div>   <div class='card-text'>   <div class='portada' style='background-image:" + data[i].image + "'>   </div>  <div class='title-total'>            <div class='title'>Work Shop Rate :" + data[i].rate + " </div>  <h2>" + data[i].name + " </h2>    <div class='address'> " + data[i].address + "</div>  <div class='actions'>             <button class='wsbtn' id='" + data[i].id + "'>  <div >Show more</div> </button>     </div>     </div>  </div>     </div>   </td> ";
-            td.innerHTML = "                      <div class='card '>  <div class='logo' style='background-image:" + data[i].icon + "'>    </div>   <div class='card-text'>   <div class='portada' style='background-image:" + data[i].image + "'>   </div>  <div class='title-total'>            <div class='title'>" + rate + "                             </div>  <h2>" + data[i].name + " </h2>    <div class='address'> " + data[i].address + "</div>   <div class='actions mt-auto btn'> <button class='wsbtn' id=' " + data[i].id + "'> <div>Show more</div>  </button>     </div>     </div>  </div>     </div>";
-
+            td.innerHTML = "                      <div class='card '>  <div class='logo' style='background-image:" + data.icon + "'>    </div>   <div class='card-text'>   <div class='portada' style='background-image:" + data.image + "'>   </div>  <div class='title-total'>            <div class='title'>" + rate + "                             </div>  <h2>" + data.name + " </h2>    <div class='address'> " + data.address + "</div>   <div class='actions mt-auto btn'> <button class='wsbtn' id=' " + data.id + "'> <div>Show more</div>  </button>     </div>     </div>  </div>     </div>";
             tr.appendChild(td);
             if (i % 2 == 1) {
                 document.getElementById("workShops").appendChild(tr);
             }
-        }
+            i++;
+        });
 
     });
+
+
 }
 
 loadData();
 
 function filterCity() {
+    i = 0;
     rate = "";
     j = 0;
     var cnt = 0;
     document.getElementById("workShops").innerHTML = "";
     city = document.getElementById("select-box1").value;
-    readTextFile("Data.json", function (text) {
-        var data = JSON.parse(text);
-        for (var i = 0; i < data.length; i++) {
+
+    db.collection("workshop").where("city", "==", city).get().then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+
+
+            var data = doc.data();
             rate = "";
             j = 0;
 
-            if (data[i].city == city) {
 
-                if (cnt % 2 == 0) {
-                    var tr = document.createElement('div');
-                    tr.className = "row";
-                }
-
-                var td = document.createElement('div');
-                td.className = "col-md-6 WshopTd";
-
-                for (j = 0; j < parseInt(data[i].rate); j++)
-                    rate += "<i class='fa fa-star rate'></i>"
-                if (data[i].rate % 1 != 0) {
-                    rate += "<i class='fa fa-star-half-empty rate'></i>"
-                    j++;
-                }
-                for (j; j < 5; j++)
-                    rate += "<i class='fa fa-star-o rate'></i>"
-
-                td.innerHTML = "  <div class='card'>   <div class='logo' style='background-image:" + data[i].icon + "'>    </div>   <div class='card-text'>   <div  class='portada' style='background-image:" + data[i].image + "'>   </div>  <div class='title-total'>  <div class='title'>" + rate + " </div>  <h2>" + data[i].name + " </h2>    <div class='address'> " + data[i].address + "</div>  <div class='actions mt-auto btn'>    <button class='wsbtn' id='" + data[i].id + " .btn'><div  >Show more</div></button>     </div>     </div>  </div>     </div>    ";
-
-                tr.appendChild(td);
-                if (cnt % 2 == 1) {
-                    document.getElementById("workShops").appendChild(tr);
-                }
+            if (cnt % 2 == 0) {
+                tr = document.createElement('div');
+                tr.className = "row";
             }
-            cnt++;
-        }
 
+            var td = document.createElement('div');
+            td.className = "col-md-6 WshopTd";
+
+            for (j = 0; j < parseInt(data.rate); j++)
+                rate += "<i class='fa fa-star rate'></i>"
+            if (data.rate % 1 != 0) {
+                rate += "<i class='fa fa-star-half-empty rate'></i>"
+                j++;
+            }
+            for (j; j < 5; j++)
+                rate += "<i class='fa fa-star-o rate'></i>"
+
+            td.innerHTML = "  <div class='card'>   <div class='logo' style='background-image:" + data.icon + "'>    </div>   <div class='card-text'>   <div  class='portada' style='background-image:" + data.image + "'>   </div>  <div class='title-total'>  <div class='title'>" + rate + " </div>  <h2>" + data.name + " </h2>    <div class='address'> " + data.address + "</div>  <div class='actions mt-auto btn'>    <button class='wsbtn' id='" + data.id + " .btn'><div  >Show more</div></button>     </div>     </div>  </div>     </div>    ";
+
+            tr.appendChild(td);
+            if (cnt % 2 == 1) {
+                document.getElementById("workShops").appendChild(tr);
+            }
+
+            cnt++;
+
+            i++;
+        });
     });
 
 }
@@ -103,44 +125,52 @@ function filterCity() {
 
 function filterLoction() {
     rate = "";
+    i = 0;
     j = 0;
     var cnt = 0;
     document.getElementById("workShops").innerHTML = "";
     loc = document.getElementById("select-box2").value;
-    readTextFile("Data.json", function (text) {
-        var data = JSON.parse(text);
 
-        var tr = document.createElement('div');
-        tr.className = "row";
+    db.collection("workshop").where("location", "==", loc).get().then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
 
-        for (var i = 0; i < data.length; i++) {
+            var data = doc.data();
             rate = "";
             j = 0;
 
-            if (data[i].location == loc) {
-
-                var td = document.createElement('div');
-                td.className = "col-md-6 WshopTd";
-
-                for (j = 0; j < parseInt(data[i].rate); j++)
-                    rate += "<i class='fa fa-star rate'></i>"
-                if (data[i].rate % 1 != 0) {
-                    rate += "<i class='fa fa-star-half-empty rate'></i>"
-                    j++;
-                }
-                for (j; j < 5; j++)
-                    rate += "<i class='fa fa-star-o rate'></i>"
-
-                td.innerHTML = "  <div class='card'>   <div class='logo' style='background-image:" + data[i].icon + "'>    </div>   <div class='card-text'>   <div  class='portada' style='background-image:" + data[i].image + "'>   </div>  <div class='title-total'>  <div class='title'>" + rate + " </div>  <h2>" + data[i].name + " </h2>    <div class='address'> " + data[i].address + "</div>  <div class='actions mt-auto btn'>    <button class='wsbtn' id='" + data[i].id + "'><div  >Show more</div></button>     </div>     </div>  </div>     </div>    ";
-                td.innerHTML = "  <div class='card'>   <div class='logo' style='background-image:" + data[i].icon + "'>    </div>   <div class='card-text'>   <div  class='portada' style='background-image:" + data[i].image + "'>   </div>  <div class='title-total'>  <div class='title'>" + rate + " </div>  <h2>" + data[i].name + " </h2>    <div class='address'> " + data[i].address + "</div>  <div class='actions mt-auto btn'>    <button class='wsbtn' id='" + data[i].id + "'><div  >Show more</div></button>     </div>     </div>  </div>     </div>    ";
-
-                tr.appendChild(td);
-
-                document.getElementById("workShops").appendChild(tr);
+            if (cnt % 2 == 0) {
+                tr = document.createElement('div');
+                tr.className = "row";
             }
 
-            cnt++;
-        }
+            var td = document.createElement('div');
+            td.className = "col-md-6 WshopTd";
 
+            for (j = 0; j < parseInt(data.rate); j++)
+                rate += "<i class='fa fa-star rate'></i>"
+            if (data.rate % 1 != 0) {
+                rate += "<i class='fa fa-star-half-empty rate'></i>"
+                j++;
+            }
+            for (j; j < 5; j++)
+                rate += "<i class='fa fa-star-o rate'></i>"
+
+            td.innerHTML = "  <div class='card'>   <div class='logo' style='background-image:" + data.icon + "'>    </div>   <div class='card-text'>   <div  class='portada' style='background-image:" + data.image + "'>   </div>  <div class='title-total'>  <div class='title'>" + rate + " </div>  <h2>" + data.name + " </h2>    <div class='address'> " + data.address + "</div>  <div class='actions mt-auto btn'>    <button class='wsbtn' id='" + data.id + "'><div  >Show more</div></button>     </div>     </div>  </div>     </div>    ";
+            tr.appendChild(td);
+
+            document.getElementById("workShops").appendChild(tr);
+
+            cnt++;
+        });
     });
+
+    // readTextFile("Data.json", function (text) {
+    // });
 }
+
+
+// db.collection("workshop").get().then((querySnapshot) => {
+//   querySnapshot.forEach((doc) => {
+//     console.log(`${doc.id} => ${JSON.stringify(doc.data())}`);
+//   });
+// });
